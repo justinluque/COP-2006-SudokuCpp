d4fdfc42c700b7b9c2bb88bbde4acf0a89999df9
Justin Luque <justin.luque@gmail.com>
2024-10-31 10:00:13 -0400

    fix: correct off by one error and lack of index incrementation

--- src/SudokuPuzzle.cpp
+++ src/SudokuPuzzle.cpp
@@ -12,23 +12,24 @@ SudokuPuzzle::SudokuPuzzle()
 // Created for debugging purposes and is only intended to be used for such
 SudokuPuzzle::SudokuPuzzle(std::initializer_list<std::initializer_list<int>> grid)
 {
-  int row_index = 0, col_index = 0;
+  int row_index = 1, col_index = 1;
   for (const auto &row : grid)
   {
-    if (row_index >= 9)
+    if (row_index > 9)
       throw std::out_of_range("Too many rows to construct 9x9 grid");
     for (const int &num : row)
     {
-      if (col_index >= 9)
+      if (col_index > 9)
         throw std::out_of_range("Too many columns to construct 9x9 grid");
       this->setCellValue(num, row_index, col_index);
+      col_index++;
     }
-    if (col_index < 8)
+    if (col_index < 9)
       throw std::out_of_range("Too few columns to construct 9x9 grid");
-    col_index = 0;
+    col_index = 1;
     row_index++;
   }
-  if (row_index < 8)
+  if (row_index < 9)
     throw std::out_of_range("Too few rows to construct 9x9 grid");
 }
 

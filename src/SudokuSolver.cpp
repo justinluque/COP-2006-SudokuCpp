9eb8f402ea1afba8e103e8ac980c6693a4080cb1
Justin Luque <justin.luque@gmail.com>
2024-10-31 11:17:47 -0400

    ref: add return to `SudokuSolver` to supress warnings for the time being

--- src/SudokuSolver.cpp
+++ src/SudokuSolver.cpp
@@ -4,10 +4,18 @@
 
 std::unique_ptr<SudokuPuzzle> SudokuSolver::solveBacktracking(const SudokuPuzzle *puzzle)
 {
+  std::unique_ptr<SudokuPuzzle> solvedPuzzle = std::make_unique<SudokuPuzzle>(*puzzle);
+
   // TODO: stuff here
+
+  return solvedPuzzle;
 }
 
 std::unique_ptr<SudokuPuzzle> SudokuSolver::solveBruteForce(const SudokuPuzzle *puzzle)
 {
+  std::unique_ptr<SudokuPuzzle> solvedPuzzle = std::make_unique<SudokuPuzzle>(*puzzle);
+
   // TODO: stuff here
+
+  return solvedPuzzle;
 }

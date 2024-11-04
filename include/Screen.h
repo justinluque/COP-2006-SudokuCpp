de68b856ea1c89b02d08a5fea18a62eb7a443659
mrtoodamnjustin <justin.luque@gmail.com>
2024-11-04 11:13:17 -0500

    ref: trim `Screen` declaration

--- include/Screen.h
+++ include/Screen.h
@@ -1,16 +1,10 @@
 #pragma once
 
-#include "ncurses/ncurses.h"
-
 class Screen
 {
 public:
-  Screen();
-  ~Screen();
+  virtual ~Screen() = default;
 
   virtual void refresh();
   virtual void handleInput();
-
-private:
-  WINDOW *window;
 };

8a2e8e18674c36bde552bc49a75e6da1b727b821
mrtoodamnjustin <justin.luque@gmail.com>
2024-11-05 17:18:19 -0500

    docs: add todo comments containing methods to be implemented

--- src/App/PuzzleScreen.cpp
+++ src/App/PuzzleScreen.cpp
@@ -1,5 +1,11 @@
 #include "PuzzleScreen.h"
 
+//  void generatePuzzle(); // CTRL_N
+//   void checkPuzzle();    // CTRL_C
+//   void solvePuzzle();    // CTRL_S
+//   void giveHint();       // CTRL_H
+//   void quit();           // CTRL_Q
+
 PuzzleScreen::PuzzleScreen()
 {
   int maxY, maxX;
@@ -23,3 +29,56 @@ PuzzleScreen::~PuzzleScreen()
 void PuzzleScreen::refresh() {}
 
 void PuzzleScreen::handleInput() {}
+
+// void App::handleInput()
+// {
+//   int key = getch();
+//   switch (key)
+//   {
+//   case CTRL_N:
+//     generatePuzzle();
+//     break;
+
+//   case CTRL_C:
+//     checkPuzzle();
+//     break;
+
+//   case CTRL_S:
+//     solvePuzzle();
+//     break;
+
+//   case CTRL_H:
+//     giveHint();
+//     break;
+
+//   case CTRL_X:
+//     quit();
+//     break;
+
+//   // TODO: define arrow key behavior
+//   case KEY_UP:
+//     break;
+
+//   case KEY_DOWN:
+//     break;
+
+//   case KEY_LEFT:
+//     break;
+
+//   case KEY_RIGHT:
+//     break;
+
+//   case '0':
+//   case '1':
+//   case '2':
+//   case '3':
+//   case '4':
+//   case '5':
+//   case '6':
+//   case '7':
+//   case '8':
+//   case '9':
+//     currentPuzzle->setCellValue(key - '0', currentRow, currentCol);
+//     break;
+//   }
+// }

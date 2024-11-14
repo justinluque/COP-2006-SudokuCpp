626ef99942ab7646acf9c5cc024683113fa2bedc
mrtoodamnjustin <justin.luque@gmail.com>
2024-11-13 23:32:59 -0500

    feat: create random choice from vector method in Random utillity namesapce

--- include/Utility/Random.h
+++ include/Utility/Random.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#include <vector>
+
 namespace Random
 {
   int randint(int min, int max);
+
+  int choose(std::vector<int> things);
 }
--- src/Utility/Random.cpp
+++ src/Utility/Random.cpp
@@ -23,3 +23,10 @@ int Random::randint(int min, int max)
 
   return min + std::rand() % ((max + 1) - min);
 }
+
+int Random::choose(std::vector<int> things)
+{
+  seed();
+
+  return things[Random::randint(0, things.size() - 1)];
+}

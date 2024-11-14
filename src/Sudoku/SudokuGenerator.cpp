195d33fd25709454e49c4f6ccaa3546f44724a85
mrtoodamnjustin <justin.luque@gmail.com>
2024-11-13 23:42:36 -0500

    feat: finish implementation of SudokuGenerator (still relies on SudokuSolver to function)

--- src/Sudoku/SudokuGenerator.cpp
+++ src/Sudoku/SudokuGenerator.cpp
@@ -1,10 +1,46 @@
 #include "SudokuGenerator.h"
 
+#include <vector>
+
+#include "SudokuSolver.h"
+#include "Random.h"
+
 std::unique_ptr<SudokuPuzzle> SudokuGenerator::generateEasy()
 {
   std::unique_ptr<SudokuPuzzle> easyPuzzle = std::make_unique<SudokuPuzzle>();
 
-  // TODO: fill the puzzle
+  std::vector<int> startingNums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+  for (int i = 0; i < 9; i++)
+  {
+    int choice = Random::randint(0, startingNums.size() - 1);
+    easyPuzzle->setCellValue(startingNums[i], 0, i);
+    startingNums.erase(startingNums.begin() + choice);
+  }
+
+  easyPuzzle = SudokuSolver::solveBacktracking(easyPuzzle);
+
+  for (int i = 0; i < 5; i++)
+  {
+    int randomRow = Random::randint(0, 8);
+    int randomCol = Random::randint(0, 8);
+
+    if (easyPuzzle->getCellValue(randomRow, randomCol) != 0)
+      easyPuzzle->strongSetValue(0, randomRow, randomCol);
+    else
+      i--;
+  }
+
+  for (int row = 0; row < 9; row++)
+  {
+    for (int col = 0; col < 9; col++)
+    {
+      if (easyPuzzle->getCellValue(row, col) == 0)
+      {
+        easyPuzzle->setFixed(true, row, col);
+      }
+    }
+  }
 
   return easyPuzzle;
 }
@@ -13,7 +49,38 @@ std::unique_ptr<SudokuPuzzle> SudokuGenerator::generateMedium()
 {
   std::unique_ptr<SudokuPuzzle> mediumPuzzle = std::make_unique<SudokuPuzzle>();
 
-  // TODO: fill the puzzle
+  std::vector<int> startingNums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+  for (int i = 0; i < 9; i++)
+  {
+    int choice = Random::randint(0, startingNums.size() - 1);
+    mediumPuzzle->setCellValue(startingNums[i], 0, i);
+    startingNums.erase(startingNums.begin() + choice);
+  }
+
+  mediumPuzzle = SudokuSolver::solveBacktracking(mediumPuzzle);
+
+  for (int i = 0; i < 9; i++)
+  {
+    int randomRow = Random::randint(0, 8);
+    int randomCol = Random::randint(0, 8);
+
+    if (mediumPuzzle->getCellValue(randomRow, randomCol) != 0)
+      mediumPuzzle->strongSetValue(0, randomRow, randomCol);
+    else
+      i--;
+  }
+
+  for (int row = 0; row < 9; row++)
+  {
+    for (int col = 0; col < 9; col++)
+    {
+      if (mediumPuzzle->getCellValue(row, col) == 0)
+      {
+        mediumPuzzle->setFixed(true, row, col);
+      }
+    }
+  }
 
   return mediumPuzzle;
 }
@@ -22,7 +89,38 @@ std::unique_ptr<SudokuPuzzle> SudokuGenerator::generateHard()
 {
   std::unique_ptr<SudokuPuzzle> hardPuzzle = std::make_unique<SudokuPuzzle>();
 
-  // TODO: fill the puzzle
+  std::vector<int> startingNums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+  for (int i = 0; i < 9; i++)
+  {
+    int choice = Random::randint(0, startingNums.size() - 1);
+    hardPuzzle->setCellValue(startingNums[i], 0, i);
+    startingNums.erase(startingNums.begin() + choice);
+  }
+
+  hardPuzzle = SudokuSolver::solveBacktracking(hardPuzzle);
+
+  for (int i = 0; i < 9; i++)
+  {
+    int randomRow = Random::randint(0, 8);
+    int randomCol = Random::randint(0, 8);
+
+    if (hardPuzzle->getCellValue(randomRow, randomCol) != 0)
+      hardPuzzle->strongSetValue(0, randomRow, randomCol);
+    else
+      i--;
+  }
+
+  for (int row = 0; row < 9; row++)
+  {
+    for (int col = 0; col < 9; col++)
+    {
+      if (hardPuzzle->getCellValue(row, col) == 0)
+      {
+        hardPuzzle->setFixed(true, row, col);
+      }
+    }
+  }
 
   return hardPuzzle;
 }

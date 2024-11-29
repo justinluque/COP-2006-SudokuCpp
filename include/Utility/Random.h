2fef4cdd0962606ced566fe8e78028d4147aa3ad
Justin Luque <justin.luque@gmail.com>
2024-11-28 19:16:57 -0500

    feat: create template function `Random::choose(std::vector<T> things)`

--- include/Utility/Random.h
+++ include/Utility/Random.h
@@ -7,5 +7,8 @@ namespace Random
   int randint(int min, int max);
 
   template <typename T>
+  T choose(std::vector<T> &things);
+
+  template <typename T>
   void shuffle(std::vector<T> &things);
 }
--- src/Utility/Random.cpp
+++ src/Utility/Random.cpp
@@ -2,6 +2,8 @@
 
 #include <random>
 #include <algorithm>
+#include <stdexcept>
+#include <utility>
 #include <ctime>
 
 namespace
@@ -28,6 +30,19 @@ int Random::randint(int min, int max)
 }
 
 template <typename T>
+T Random::choose(std::vector<T> &things)
+{
+  if (things.empty())
+    throw std::invalid_argument("Cannot choose from an empty vector.");
+
+  seed();
+
+  std::uniform_int_distribution<std::size_t> distribution(0, things.size() - 1);
+
+  return things[distribution(rng)];
+}
+
+template <typename T>
 void Random::shuffle(std::vector<T> &things)
 {
   seed();
@@ -35,5 +50,6 @@ void Random::shuffle(std::vector<T> &things)
   std::shuffle(things.begin(), things.end(), rng);
 }
 
-// Explicitly instantiate template
+// Explicitly instantiate templates
 template void Random::shuffle(std::vector<int> &things);
+template std::pair<int, int> Random::choose(std::vector<std::pair<int, int>> &things);
